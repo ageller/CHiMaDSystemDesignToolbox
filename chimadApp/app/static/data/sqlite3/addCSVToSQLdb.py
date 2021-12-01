b43089c0642683eab4ef8305074fd6e12cdf7826
Aaron Geller <a-geller@northwestern.edu>
2021-12-01 11:03:19 -0600

    working on sqlite3 db

---
+++ chimadApp/app/static/data/sqlite3/addCSVToSQLdb.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sqlite3
+import re
+
+def addCSVToSQLdb(dbName, csvName):
+	
+	# read in the csv file
+	df = pd.read_csv('../' + csvName)
+
+	# connect to the SQL database (this will create a file if it does not already exist)
+	conn = sqlite3.connect(dbName)
+	cursor = conn.cursor()
+	
+	# the table name within the databse will be the csv file name w/o .csv
+	# and take only alphanumeric values
+	tableName = re.sub(r'\W+', '',csvName.replace('.csv','')) 
+
+	# include all columns, and assume that they are all text
+	# allow only alphanumeric values in column names
+	cols = ''
+	for col in df.columns:
+		cc = re.sub(r'\W+', '', col) 
+		cols += cc + ' text, ' 
+	cols = cols[:-2]
+
+	# create the table
+	cursor.execute('CREATE TABLE IF NOT EXISTS ' + tableName + ' (' + cols + ')')
+	conn.commit()
+	
+	# add the dataFrame into the database
+	df.to_sql(tableName, conn, if_exists='replace', index = False)
